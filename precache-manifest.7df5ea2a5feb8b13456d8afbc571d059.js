34a0e7bf94f30245bff48ea6614d690e03918028
Alexsavchuk95 <savchukalex57@gmail.com>
2019-12-17 20:56:50 +0300

    Updates

---
+++ precache-manifest.7df5ea2a5feb8b13456d8afbc571d059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2119430f5a8d079b1b70aa2d8063b89",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "97b8a1c4e9edf3c865ac",
+    "url": "/goit-react-hw-05-budget-planner/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "5d4fad882dc3994f260b",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "97b8a1c4e9edf3c865ac",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.adc5e95b.chunk.js"
+  },
+  {
+    "revision": "5d4fad882dc3994f260b",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.b1b6a005.chunk.js"
+  },
+  {
+    "revision": "05df1f71193a6b1ae3a5",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.17532924.js"
+  }
+]);
